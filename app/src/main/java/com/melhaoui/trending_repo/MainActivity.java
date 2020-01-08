8603aade8ad22209fb9b26dc5e2c3a9a2cd98014
eliasmliha <eliasmlhaoui@gmail.com>
2020-01-08 18:40:06 +0000

    create item repo layout for recyclerview

--- app/src/main/java/com/melhaoui/trending_repo/MainActivity.java
+++ app/src/main/java/com/melhaoui/trending_repo/MainActivity.java
@@ -12,10 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 public class MainActivity extends AppCompatActivity {
 
